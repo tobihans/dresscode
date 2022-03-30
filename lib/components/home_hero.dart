371ca63754443ee5802535e4245a290d1726757b
Hans Tognon <learningway403@gmail.com>
2022-03-30 22:05:30 +0100

    dev: just to save work

---
+++ lib/components/home_hero.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomeHero extends StatelessWidget {
+  const HomeHero({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      heightFactor: 0.25,
+      widthFactor: 1,
+      child: Container(
+        child: Align(
+          alignment: Alignment.bottomRight,
+          child: TextButton(
+              onPressed: _heroButtonPressed, child: const Text('Voir')),
+        ),
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(
+                    'https://source.unsplash.com/random/1600x900?mode&dress&style&beautiful&sig=1'))),
+      ),
+    );
+  }
+
+  void _heroButtonPressed() {}
+}
