2f4320becf39d199113d2ffe0f12983442c5bbf1
iamklaus <iamklaus01@gmail.com>
2022-04-28 07:36:16 +0100

    Wishlist Page created - Wishlist Route added - A special product card has been built - The appBar has to be changed if the new one with search bar is implemented

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -4,4 +4,6 @@ class Routes {
   static const String details = '/details';
   static const String login = '/login';
   static const String register = '/register';
+  static const String wishlist = '/wishlist';
+
 }
