dceb42129c8e080a95c3b8c8c1e99576ecc663ba
Hans Tognon <learningway403@gmail.com>
2022-04-28 23:24:56 +0100

    feat: finish home page design; needs connector to be fulfilled.

--- lib/components/product_card.dart
+++ lib/components/product_card.dart
@@ -6,23 +6,49 @@ import 'package:flutter/material.dart';
 
 /// TODO
 class ProductCard extends StatelessWidget {
-  final Product product;
-  final ProductService productService;
-  final CartService cartService;
-  final WishlistService wishlistService;
+  // final Product product;
+  // final ProductService productService;
+  // final CartService cartService;
+  // final WishlistService wishlistService;
 
   const ProductCard({
     Key? key,
-    required this.product,
-    required this.productService,
-    required this.cartService,
-    required this.wishlistService,
+    // required this.product,
+    // required this.productService,
+    // required this.cartService,
+    // required this.wishlistService,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Text('ProductCard'),
-    );
+    return Container(
+        margin: const EdgeInsets.symmetric(horizontal: 5.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 128.0,
+              width: 128.0,
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5.0),
+                    image: const DecorationImage(
+                        image: NetworkImage(
+                            "https://source.unsplash.com/random/1600x900?mode&clothe&dress&style&beautiful&sig=10"),
+                        fit: BoxFit.cover)),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 2.5, left: 2.5),
+              child: const Text('Blablabla',
+                  style: TextStyle(fontWeight: FontWeight.w700)),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 2.5, left: 2.5),
+              child: const Text('2555 XOF'),
+            ),
+          ],
+        ));
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,6 +4,8 @@ import 'package:dresscode/components/app_bar.dart';
 import 'package:dresscode/components/floating_btn.dart';
 import 'package:dresscode/components/home_hero.dart';
 import 'package:dresscode/components/category.dart';
+import 'package:dresscode/components/top_model.dart';
+import 'package:dresscode/components/product_card.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -41,6 +43,31 @@ class _HomeScreenState extends State<HomeScreen> {
                             "https://source.unsplash.com/random/1600x900?mode&clothe&dress&style&beautiful&sig=10$e"))
                     .toList(),
               )),
+          Container(
+              padding: const EdgeInsets.symmetric(vertical: 6.0),
+              child: const Text('Les top modèles')),
+          SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(vertical: 6.0),
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(9, (i) => i)
+                    .map((e) => TopModel(
+                        name: "Jan$e",
+                        url:
+                            "https://source.unsplash.com/random/1600x900?mode&clothe&dress&style&beautiful&sig=10$e"))
+                    .toList(),
+              )),
+          Container(
+              padding: const EdgeInsets.symmetric(vertical: 6.0),
+              child: const Text('Les top modèles')),
+          SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(vertical: 6.0),
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(9, (i) => i)
+                    .map((e) => const ProductCard())
+                    .toList(),
+              ))
         ],
       ),
     );
