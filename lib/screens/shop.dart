e7c0303668eec91650b5f6b4d7616ddfb9644ec7
Kpanou Gilles <76532932+Gilles-kpn@users.noreply.github.com>
2022-05-12 21:07:47 +0100

    adding view model to shop

--- lib/screens/shop.dart
+++ lib/screens/shop.dart
@@ -1,6 +1,11 @@
+import 'package:dresscode/api/services/cart_service.dart';
+import 'package:dresscode/api/services/product_service.dart';
+import 'package:dresscode/api/services/product_service.dart';
+import 'package:dresscode/api/services/wishlist_service.dart';
 import 'package:dresscode/components/app_bar.dart';
 import 'package:dresscode/components/app_drawer.dart';
 import 'package:dresscode/components/floating_btn.dart';
+import 'package:dresscode/utils/token_storage.dart';
 import 'package:flutter/material.dart';
 
 class ShopScreen extends StatefulWidget {
@@ -13,6 +18,24 @@ class ShopScreen extends StatefulWidget {
 }
 
 class _ShopScreenState extends State<ShopScreen> {
+  Future<ShopViewModel> _initData() async {
+    final token = await TokenStorage.getToken();
+    final productService = ProductService(token);
+    final wishlistService = WishlistService(token);
+    final cartService = CartService(token);
+    final wishlist = await wishlistService.getWishlist();
+    return ShopViewModel(
+      productService: productService,
+      cartService: cartService,
+      wishlistService: wishlistService,
+    );
+  }
+
+
+  @override
+  void initState(){
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,4 +48,16 @@ class _ShopScreenState extends State<ShopScreen> {
       body: Material(),
     );
   }
+}
+
+class ShopViewModel{
+  final ProductService productService;
+  final CartService cartService;
+  final WishlistService wishlistService;
+
+  ShopViewModel({
+    required this.productService,
+    required this.cartService,
+    required this.wishlistService
+  });
 }
