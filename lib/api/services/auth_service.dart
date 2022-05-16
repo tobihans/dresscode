c6aefaa1ec8bd25a7ecf1d68bd7d3e84eb18a30b
olabijed@gmail.com <olabijed@gmail.com>
2022-05-16 07:16:36 +0100

    Account update and password update implemented

--- lib/api/services/auth_service.dart
+++ lib/api/services/auth_service.dart
@@ -4,29 +4,14 @@ import 'package:dresscode/api/core/api_base.dart';
 import 'package:dresscode/api/core/api_http_exception.dart';
 import 'package:dresscode/api/core/constants.dart';
 import 'package:dresscode/models/user.dart';
+import 'package:dresscode/requests/account_update_request.dart';
 import 'package:dresscode/requests/login_request.dart';
+import 'package:dresscode/requests/password_update_request.dart';
 import 'package:dresscode/requests/register_request.dart';
 import 'package:dresscode/utils/token_storage.dart';
 
 class AuthService extends ApiBase {
   static User? _currentUser;
-
-  Future<bool> register(RegisterRequest registerRequest) async {
-    final registerResponse = await post(
-      Uri.parse(Constants.registerUrl),
-      data: registerRequest.toJson(),
-    );
-    return jsonDecode(registerResponse)['message'] ==
-        'User created successfully';
-  }
-
-  Future<String> login(LoginRequest loginRequest) async {
-    final loginResponse = await post(
-      Uri.parse(Constants.loginUrl),
-      data: loginRequest.toJson(),
-    );
-    return (jsonDecode(loginResponse)['content']).toString().split(' ').last;
-  }
 
   Future<User?> getCurrentUser(String token) async {
     if (_currentUser == null) {
@@ -45,11 +30,49 @@ class AuthService extends ApiBase {
     return _currentUser;
   }
 
-  Future<void> logout() async {
-    _currentUser = null;
-  }
-
   static Future<User?> getUserFromTokenStorage() async {
     return await (AuthService().getCurrentUser(await TokenStorage.getToken()));
+  }
+
+  Future<bool> register(RegisterRequest registerRequest) async {
+    final registerResponse = await post(
+      Uri.parse(Constants.registerUrl),
+      data: registerRequest.toJson(),
+    );
+    return jsonDecode(registerResponse)['message'] ==
+        'User created successfully';
+  }
+
+  Future<String> login(LoginRequest loginRequest) async {
+    final loginResponse = await post(
+      Uri.parse(Constants.loginUrl),
+      data: loginRequest.toJson(),
+    );
+    return (jsonDecode(loginResponse)['content']).toString().split(' ').last;
+  }
+
+  Future<void> updateUserAccount(
+      AccountUpdateRequest accountUpdateRequest, String token) async {
+    final updateResponse = await put(Uri.parse(Constants.accountUpdateUrl),
+        data: accountUpdateRequest.toJson(), token: token);
+    final user =
+        User.fromJson(jsonEncode(jsonDecode(updateResponse)['content']));
+    _currentUser = user;
+  }
+
+  Future<bool> updateUserPassword(
+    PasswordUpdateRequest passwordUpdateRequest,
+    String token,
+  ) async {
+    final updatePasswordResponse = await put(
+      Uri.parse(Constants.passwordUpdateUrl),
+      data: passwordUpdateRequest.toJson(),
+      token: token,
+    );
+    return jsonDecode(updatePasswordResponse)['content'] == 'Password changed';
+  }
+
+  Future<void> logout() async {
+    _currentUser = null;
   }
 }
